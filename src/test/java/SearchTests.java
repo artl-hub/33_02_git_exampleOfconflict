6f7953854fecfeaa345e64b005090fdb46d15586
artl <artemlarichev.v@gmail.com>
2025-01-08 20:23:33 +0100

    comments deleted

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,16 +6,16 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        //Открыть google
+
         open("https://www.google.com/");
 
-        //Отказаться от coockie
+
         $("#W0wltc > div").click();
 
-        // Ввести "selenide" в поиск
+
         $("[name=q]").setValue("selenide").pressEnter();
 
-        //Проверить что selenide появился в результатах поиска
+
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 
